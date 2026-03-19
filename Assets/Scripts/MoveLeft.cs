6dcd5e81caab6efb17b1cb4e0f6f38c2b99a453c
Chanyut Leecharoen <30559766+chanyutx10@users.noreply.github.com>
2026-03-20 06:15:07 +0700

    Week10 - Examination

---
+++ Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    public float speed = 10f;
+
+    private float leftBound = -15;
+
+    private PlayerController playerController;
+
+    void Start()
+    {
+        playerController = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playerController.gameOver)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * speed);
+        }
+
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
